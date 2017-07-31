2596ef2a056c98b7352cf7dae96159df4973a47f
Jason Rowland <jasonrowland86>
2017-07-31 16:20:46 -0400

    Added app.js and config.js

--- app.js
+++ app.js
@@ -1,4 +1,62 @@
-var mma = require('mma');
- mma.fighter("Anderson Silva", function(data) {
-    console.log(data);
-  });
+//import all dependencies
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+// const methodOverride = require('method-override');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const passport = require('passport');
+
+//initialize app
+const app = express();
+
+// require('dotenv').config();
+
+//middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(methodOverride('_method'));
+// app.use(cookieParser());
+// app.use(session({
+//   secret: process.env.SECRET_KEY,
+//   resave: false,
+//   saveUninitialized: true,
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+//static files
+app.use(express.static('public'));
+
+//views
+// app.set('views', path.join(__dirname, '/views'));
+// app.set('view engine', 'ejs');
+
+//set the port
+const PORT = process.env.PORT || 3000;
+//tell app to use port
+app.listen(PORT, ()=>{
+  console.log(`On port ${PORT}`);
+});
+
+//index route
+app.get('/', (req, res)=>{
+  res.send('hello world');
+  // res.render('index', {
+  //   currentPage: 'Index',
+  //   documentTitle: 'MMA Betting Dashboard',
+  // });
+});
+//views routes
+// const dashboardRoutes = require('./routes/dashboard-routes');
+// app.use('/dashboard', dashboardRoutes);
+// const authRoutes = require('./routes/auth-routes');
+// app.use('/auth', authRoutes);
+// const userRoutes = require('./routes/user-routes');
+
+//error handler
+app.get('*', (req, res) => {
+    res.status(404).send('not found!');
+});
